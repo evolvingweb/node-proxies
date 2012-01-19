9a9f418b9ff5bec8c92ecbcd42ab0a9c008a8458
Thomas Getgood <thomas.getgood@gmail.com>
2012-01-19 14:52:50 -0500

    Cleaned up comment header in solr proxy.

--- solrProxy.js
+++ solrProxy.js
@@ -5,13 +5,35 @@
 // but more generally applicable anyone wishing to expose Solr to the outside
 // world.
 //
+// Info:
+// ============================================================================
 // Created: 09/01/2012
-// Last modified: 13/01/2012
-//
+// Last modified: 19/01/2012
+// Version: 0.0.?
 // Author: Thomas Getgood <thomas@evolvingweb.ca>
+//
+// Usage:
+// ============================================================================
+//
+// Say this proxy is running a machine whose IP is a.b.c.d: configure Solr to
+// accept requests on port 8080 from a.b.c.d and nowhere else, then redirect
+// all web traffic through a.b.c.d:8008. 
+// 
+// N.B. If you are already using a Solr instance which takes requests
+// optimistically from the internet, you need to reconfigure it to only accept
+// requests from the server this proxy is running on. The proxy won't help you
+// if someone can bypass it.
+//
+// For instance, if you're running Solr on Tomcat, with this proxy on the same
+// machine, then add the line: 
+//
+// <Valve className="org.apache.catalina.valves.RemoteAddrValve" allow="127\.0\.0\.1"/>
+//
+// to /etc/solr/solr-tomcat.xml. 
 
 
 // Filter applied by proxy:
+// ============================================================================
 //
 // Very restrictive. Loosen with care.
 //
@@ -21,16 +43,24 @@
 // Disable the 'commit' parameter
 // only allow access to /solr/select
 // 100 rows max per query
+// ============================================================================
 
 
 // TODO:
-// * log IPs of offending queries.
+// * log IPs of offending queries (to an actual log file).
 // * log intialisation time so that we can see when it goes down.
+// * Make sure document access permissions can be enforced.
+// * What to do if we need to allow custom handlers?
+// * Allow solr path --- i.e. xxx in /xxx/select --- to be configured.
 
 
 var httpProxy = require('http-proxy'),
     url = require('url');
 
+/* 
+ * Filter function. Returns true if the query object fulfils the
+ * requirements set out above, false otherwise. Could be a bit clearer...
+ */
 var querySafe = function (params) {
   var query = path.query;
   if (params.pathname === '/solr/select' &&
